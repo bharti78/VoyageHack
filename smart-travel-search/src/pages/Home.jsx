0169c06c0821fea6a044682808e481fa0e6e4233
saloni verma <saloniv375@gmail.com>
2026-02-23 21:47:39 +0530

    cab

--- smart-travel-search/src/pages/Home.jsx
+++ smart-travel-search/src/pages/Home.jsx
@@ -14,6 +14,7 @@ const Home = () => {
     }
   }, []);
 
+  
   return (
     <div className="min-h-screen bg-cover bg-center bg-no-repeat" style={{ backgroundImage: 'url(/travel.jpeg)' }}>
       <div className="min-h-screen bg-black bg-opacity-40">
