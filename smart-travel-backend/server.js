1d366449c263ea34b578ffac240d0733c0b67545
Bharti Dhote <2023kucp1049@iiitkota.ac.in>
2026-02-28 21:58:20 +0530

    live done

--- smart-travel-backend/server.js
+++ smart-travel-backend/server.js
@@ -32,6 +32,10 @@ app.use(cors({
 app.use(express.json({ limit: '10mb' }))
 app.use(express.urlencoded({ limit: '10mb', extended: true }))
 
+app.get("/ping", (req, res) => {
+  res.send("pong");
+});
+
 app.use("/api/auth", require("./routes/authRoutes"))
 app.use("/api/search", require("./routes/searchRoutes"))
 app.use("/api/user", require("./routes/userRoutes"))
